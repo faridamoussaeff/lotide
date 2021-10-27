e149f96250ae670bf6dd0e07fce1c03efc484ae2
Farida <faridamus@protonmail.com>
2021-10-27 18:16:03 +0000

    Update files

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,3 @@
-//Instructions: 
-// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-// Use assertEqual to write test cases for various scenarios. 
-
-// Expected Outcome: 
-/*
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-*/
-
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -37,14 +23,4 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true);
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), false);
 assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 4]), true);
-
-
-//Output: 
-/*true
-false
-✅✅✅ Assertion Passed: true === true
-🛑🛑🛑 Assertion Failed: true !== false
-✅✅✅ Assertion Passed: false === false
-🛑🛑🛑 Assertion Failed: false !== true
-*/ 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
