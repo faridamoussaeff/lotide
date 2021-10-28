d5ed32cab0179f593a5c6dbd5d85f62adfea6e54
Farida <faridamus@protonmail.com>
2021-10-28 02:56:43 +0000

    Implement middle problem

---
+++ middle.js
@@ -0,0 +1,46 @@
+//Challenge: 
+//Implement middle which will take in an array and return the middle-most of 
+//given array
+
+// TEST /ASSERTION FUNCTIONS
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion Passed: two arrays are equal.`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: two arrays are not equal.`);
+  }
+
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//ACTUAL FUNCTIONS
+
+const middle = function (array) {
+  let result = ''
+  let newlength = array[Math.floor(array.length / 2)];
+  if (array.length < 3) {
+    return []
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      result = array[newlength - 1] + ', ' + array[newlength]
+
+    } else {
+      result = array[Math.floor(array.length / 2)];
+    }
+  }
+  return result
+}
+console.log(middle([1]));  // prints []
+console.log(middle([1, 2])); // []
+console.log(middle([1, 2, 3])); // [2]
+console.log(middle([1, 2, 3, 4, 5])); // [3]
