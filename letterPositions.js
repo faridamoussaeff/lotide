091f79dd8d69d2788682f751440c2cac74f59add
Farida <faridamus@protonmail.com>
2021-10-29 02:58:40 +0000

    Add a complete file

--- letterPositions.js
+++ letterPositions.js
@@ -8,9 +8,6 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
       return false; 
@@ -19,19 +16,19 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true; 
 };
 
-
-const letterPositions = function(sentence) {
-  const results = {}; 
+const letterPositions = function (sentence) {
+  const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== ' ') {
-      if (results[sentence[i]]) {
-        results[sentence[i]].push(i);
-      } else {
-        results[sentence[i]] = [i];
+      if (sentence[i] !== ' ') {
+          if (results[sentence[i]]) {
+              results[sentence[i]].push(i);
+          } else {
+              results[sentence[i]] = [i];
+          }
       }
-    }
   }
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").e,[1]);
+
+assertArraysEqual(letterPositions("hello").e, [1]);
