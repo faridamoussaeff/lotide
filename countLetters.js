b10fd67ca8ddbff57284b2a1f8a534f5c4ffaef0
Farida <faridamus@protonmail.com>
2021-11-01 21:18:01 +0000

    Update countLetters.js with eslint

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,23 @@
-const assertEqual = function (actual, expected) {
-     if (actual === expected) {
-         console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-     } else {
-         console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-     }
- };
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
+  }
+};
 
- const countLetters = function (word) {
-     let result = {};
-     const newList = word.split(' ').join('').toLowerCase();
-     for (const letter of newList) {
-         if (result[letter]) {
-             result[letter] += 1;
-         } else {
-             result[letter] = 1;
-         }
-     }
-     return result;
- };
+const countLetters = function(word) {
+  let result = {};
+  const newList = word.split(' ').join('').toLowerCase();
+  for (const letter of newList) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
 
- console.log(countLetters("lighthouse in the house")) 
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("Happy LHL"));
