127cb613d478a4bc8e7e9fcc432d68ca6a78eb7e
Farida <faridamus@protonmail.com>
2021-11-01 18:39:30 +0000

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false; 
-    }
-  }
-  return true; 
-};
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -18,8 +6,25 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+const eqArrays = function(arrayOne, arrayTwo) {
+  for(let i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] === arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+//Expected output: 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true);
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), false);
 assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 4]), false);
