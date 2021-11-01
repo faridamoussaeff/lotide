4d8b047bd96285150f407ebce5533f462a1768b3
Farida <faridamus@protonmail.com>
2021-11-01 18:07:05 +0000

    update head.js

--- head.js
+++ head.js
@@ -1,10 +1,17 @@
-const assertEqual = require('./assertEqual');
+const head = function (array) {
+  //console.log(array[0]);
 
-const head = function(array) {
-  return array[0];
+  return array [0];
+}
+head([1, 2, 3, 4, 5]);
+head(["hello", "this", "is", "an", "array"]);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
-
-module.exports = head;
-
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
