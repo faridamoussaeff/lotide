545158db7453e002b26b7ae2e9fd8a559a6d1200
Farida <faridamus@protonmail.com>
2021-11-01 21:23:48 +0000

    Update countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -21,3 +21,7 @@ const countLetters = function(word) {
 
 console.log(countLetters("lighthouse in the house"));
 console.log(countLetters("Happy LHL"));
+
+//Output:
+//{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
+//{ h: 2, a: 1, p: 2, y: 1, l: 2 }
