a22147c46ee9d97f31cdf1ed7669852407b899ed
Farida <faridamus@protonmail.com>
2021-11-01 20:38:40 +0000

    Update after eslint

--- countOnly.js
+++ countOnly.js
@@ -10,8 +10,9 @@ module.exports = assertEqual;
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
-  const results = {}
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
   for (const item of allItems)
     if (itemsToCount[item]) {
