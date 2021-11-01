055a56f31f01d5e659a4c102daaf0af5ca38779d
Farida <faridamus@protonmail.com>
2021-11-01 17:51:11 +0000

    Update the assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,18 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+assertEqual("Bootcamp", "Lighthouse Labs");
+assertEqual(1, 4);
+assertEqual("House", "house");
+assertEqual("Home", "Home");
 
-module.exports = assertEqual;
+// if the values match, it should print the following: 
+//🛑🛑🛑 Assertion Failed: Bootcamp !== Lighthouse Labs
+//🛑🛑🛑 Assertion Failed: 1 !== 4
+//🛑🛑🛑 Assertion Failed: House !== house
+//✅✅✅ Assertion Passed: Home === Home
+
+
+
 
 
