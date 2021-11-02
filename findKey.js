47ec4a05e235d40fad19bc0acf0a281b609e59ed
Farida <faridamus@protonmail.com>
2021-11-02 01:23:42 +0000

    Revise and add findKey.js

--- findKey.js
+++ findKey.js
@@ -1,21 +1,3 @@
-/*
-
-Instructions:
-Implement the function findKey which takes in an object and a callback.
-It should scan the object and return the first key for which the callback
-returns a truthy value. If no key is found, then it should return undefined.
-
-Example
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-*/
-
 const assertEqual = function (actual, expected) {
     if (actual === expected) {
         console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
