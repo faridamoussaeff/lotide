598f0b977d8fce439d65f1662a782b6db1604e93
Farida <faridamus@protonmail.com>
2021-11-02 01:06:40 +0000

    Revise code and update map.js after eslint

--- map.js
+++ map.js
@@ -1,6 +1,30 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const result = eqArrays(arrayOne, arrayTwo);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: two arrays are equal.`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: two arrays are not equal.`);
+  }
+};
 
-const map = function(array, callback) {
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length > arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+  } if (arrayOne.length < arrayTwo.length) {
+     for(let i = 0; i < arrayTwo.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+  }
+}
+return true;
+}; 
+
+const map = (array, callback) => {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
@@ -8,5 +32,18 @@ const map = function(array, callback) {
   return results;
 }
 
+const words = ["ground", "control", "to", "major", "tom"];
+const names = ["sarah", "jeniffer", "blair"]
+
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+const results2 = map(names, name => name.length);
+console.log(results1);
+
+const results3 = map(names, name => name.length + 1);
+console.log(results1);
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"])
+assertArraysEqual(results2, [3, 4, 4])
+assertArraysEqual(results3, [4, 5, 5])
