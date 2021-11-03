d7ab1ff3ed5b4a880746ae760ffe8d17a4abe90a
Farida <faridamus@protonmail.com>
2021-11-03 03:01:15 +0000

    Refactor head.js

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const head = function (array) {
-  //console.log(array[0]);
+const assertEqual = require('./assertEqual');
 
+const head = function (array) {
   return array [0];
 }
-head([1, 2, 3, 4, 5]);
-head(["hello", "this", "is", "an", "array"]);
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head; 
