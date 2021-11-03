9f09d8c28f8c04a41d2d2b84b31d495d57019400
Farida <faridamus@protonmail.com>
2021-11-03 03:02:59 +0000

    Refactoring assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -5,18 +5,6 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Bootcamp", "Lighthouse Labs");
-assertEqual(1, 4);
-assertEqual("House", "house");
-assertEqual("Home", "Home");
 
-// if the values match, it should print the following: 
-//🛑🛑🛑 Assertion Failed: Bootcamp !== Lighthouse Labs
-//🛑🛑🛑 Assertion Failed: 1 !== 4
-//🛑🛑🛑 Assertion Failed: House !== house
-//✅✅✅ Assertion Passed: Home === Home
-
-
-
-
+module.exports = assertEqual;
 
