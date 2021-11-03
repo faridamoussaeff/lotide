7ebed29f96b99760cae7fb1d48da743619612fde
Farida <faridamus@protonmail.com>
2021-11-03 05:36:17 +0000

    Testing with Mocha and Chai

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+
+const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
+assertArraysEqual([0], [0]);
+assertArraysEqual([], []);
+assertArraysEqual([5, 5, 5, 5], [5, 5, 5, 5]);
